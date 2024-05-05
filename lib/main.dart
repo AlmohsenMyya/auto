c4ee985efb78659a685e423d77688326e531e5a2
almohsen myya <almohsenmyya2233@@gmail.com>
2024-05-05 09:28:14 -0400

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:auto/app/my_app_controller.dart';
 import 'package:auto/core/data/repositories/shared_preference_repository.dart';
 import 'package:auto/core/services/connectivity_service.dart';
-import 'package:auto/core/services/location_service.dart';
-import 'package:auto/core/services/notification_service.dart';
-import 'package:auto/firebase_options.dart';
-// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -21,8 +17,6 @@ Future<void> main() async {
   );
 
   Get.put(SharedPreferenceRepository());
-  //Get.put(CartService());
-  Get.put(LocationService());
   Get.put(ConnectivityService());
   Get.put(MyAppController());
 
