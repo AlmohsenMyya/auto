c4d9a8a7f2fff676dd787f2a5f01bd3adb11f1d2
D-family-Almohsen <almohsenmyya1999@gmail.com>
2024-09-05 18:25:35 +0300

    refresh data when notifications come

--- lib/core/utils/helpers/NotificationFirebase.dart
+++ lib/core/utils/helpers/NotificationFirebase.dart
@@ -5,6 +5,8 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../data/repositories/read_all_models.dart';
+
 class NotificationSetUp {
   static final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
   FlutterLocalNotificationsPlugin();
@@ -81,12 +83,14 @@ class NotificationSetUp {
       await _setUpNotifications();
 
       // التعامل مع الإشعارات عند ورودها
-      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+        await JsonReader.fetchDataAndStore();
         _showNotification(message);
       });
 
       // التعامل مع الإشعارات عند فتح التطبيق
-      FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
+        await JsonReader.fetchDataAndStore();
         _handleMessage(message);
       });
 
